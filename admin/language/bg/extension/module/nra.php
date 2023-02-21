065a539e7e4053309d67376a0f7b46ff8f9eded9
Pavel Palashturov <pavel@netreact.com>
2023-02-21 22:54:02 +0200

    Start Module

---
+++ admin/language/bg/extension/module/nra.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Генериране на XML към НАП ';
+
+// Text
+$_['text_extension']   = 'Допълнения';
+$_['text_success']     = 'Успех: Модулът успешно е обновен!';
+$_['text_edit']        = 'Редактиране';
+
+// Entry
+$_['entry_status']     = 'Статус';
+
+// Error
+$_['error_permission'] = 'Внимание: Вие нямате права да променяте настройките!';
---
+++ admin/model/extension/module/nra.php
@@ -0,0 +1,4 @@
+<?php
+class ModelExtensionModuleNra extends Model {
+
+}
